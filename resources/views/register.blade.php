b334a961b66c46f32d79b47009937e10a65ac37b
anvarbeck <saidovanvar862@gmail.com>
2025-09-23 11:13:40 +0500

    Auth

---
+++ resources/views/register.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>{{ config('app.name', 'Laravel') }}</title>
+</head>
+<body>
+    <div class="flex-col w-[50%] bg-amber-50 h-[500px]  m-auto">
+        <input type="text" name="" id="" placeholder="Enter your name">
+        <input type="email" name="" id="" placeholder="Enter your email">
+        <input type="text" name="" id="" placeholder="Enter your username">
+        <input type="password" name="" id="" placeholder="Enter your password">
+        <input type="password" name="" id="" placeholder="Confirm your password">
+    </div>
+</body>
+</html>
