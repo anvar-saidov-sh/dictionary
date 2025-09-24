d0d91096f8eed04e31a7fc8e6dcd85ce643ffa5d
anvarbeck <saidovanvar862@gmail.com>
2025-09-24 18:21:10 +0500

    Auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,6 +7,8 @@
         <input type="email" name="email" required>
         <label for="password">Password:</label>
         <input type="password" name="password" required>
+        <label for="password_confirmation">Confirm password:</label>
+        <input type="password" name="password_confirmation" required>
         <button type="submit" class="btn mt-4">Register</button>
     </form>
 </x-layout>
