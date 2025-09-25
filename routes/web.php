ac16c8a3a89439a11bb8e335d374e54408330956
anvarbeck <saidovanvar862@gmail.com>
2025-09-25 20:02:17 +0500

    Auth

--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/register', [AuthController::class, 'showRegister']);
-Route::post('/register', [AuthController::class, 'register']);
-
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
 
 Route::get('student/register', [StudentController::class, 'showRegisterForm'])->name('student.register');
 Route::post('student/register', [StudentController::class, 'register']);
