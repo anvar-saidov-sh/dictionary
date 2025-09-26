a0d7bc38374a511a85bd5c59b22d8e45e03df51a
anvarbeck <anvarsaidov732@gmail.com>
2025-09-26 19:35:14 +0500

    WORD CRUD

--- resources/views/words/create.blade.php
+++ resources/views/words/create.blade.php
@@ -1,6 +1,15 @@
-@extends('layout')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+
+<body>
     <h1>Add New Word</h1>
 
     @if ($errors->any())
@@ -33,4 +42,6 @@
 
         <button type="submit">Save Word</button>
     </form>
-@endsection
+</body>
+
+</html>
