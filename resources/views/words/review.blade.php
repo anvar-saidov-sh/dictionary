ec80dbd9856b37a4ee57ce8a61dad554f06cb4d0
anvarbeck <anvarsaidov732@gmail.com>
2025-10-01 17:58:54 +0500

    Review Blade Editing

--- resources/views/words/review.blade.php
+++ resources/views/words/review.blade.php
@@ -1,7 +1,9 @@
 <x-layout>
     <div class="max-w-3xl mx-auto mt-10 bg-white shadow-md rounded-lg p-6">
-        <h1 class="text-3xl font-bold text-indigo-700 mb-4">{{ $word->name }}</h1>
-
+        <h1 class="text-3xl font-extrabold text-indigo-700 mb-4">{{ $word->name }}</h1>
+        <p class="text-xl font-semibold text-gray-800 mb-1.5">{{$word->definition}}</p>
+        <p class="text-xl font-semibold text-gray-800 mb-1.5">{{$word->examples}}</p>
+        <p class="text-xl font-semibold text-gray-800 mb-1.5">{{$word->idioms}}</p>
         <p class="text-gray-700 mb-6">
             <span class="font-semibold">Author:</span> {{ $word->student->name ?? 'Unknown' }}
         </p>
@@ -17,13 +19,13 @@
                     @csrf
                     @method('DELETE')
                     <button type="submit"
-                        class="px-3 py-1 bg-red-500 text-white rounded-lg hover:bg-red-600 transition"
+                        class="px-3 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition"
                         onclick="return confirm('Are you sure?')">
                         Delete
                     </button>
                 </form>
             @else
-                <form action="{{ route('words.requests.create', [$letter, $word->id]) }}" method="POST" class="inline">
+                <form action="{{ route('words.requests.create', [$letter, $word->id]) }}" method="GET" class="inline">
                     @csrf
                     <button type="submit"
                         class="px-3 py-2  bg-purple-400 text-white rounded-lg hover:bg-purple-500 transition">
