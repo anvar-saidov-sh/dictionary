7d1392d2dcb16b222a8a60113076db968fc82880
anvarbeck <anvarsaidov732@gmail.com>
2025-10-01 17:53:55 +0500

    Routing

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,7 @@ Route::post('/login', [StudentController::class, 'login']);
 Route::post('/logout', [StudentController::class, 'logout'])->name('logout');
 
 Route::middleware('auth:student')->group(function () {
-    Route::get('/dashboard', function () {
-        $user = auth()->guard('student')->user();
-        $words = $user->words()->latest()->get();
-        return view('students.dashboard', compact('user', 'words'));
-    })->name('dashboard');
+    Route::get('/dashboard', [StudentController::class, 'dashboard'])->name('dashboard');
     Route::get('/words', [WordsController::class, 'index'])->name('index');
     Route::get('/words/create', [WordsController::class, 'create'])->name('words.create');
     Route::post('/words', [WordsController::class, 'store'])->name('words.store');
@@ -38,4 +34,6 @@ Route::middleware('auth:student')->group(function () {
     Route::delete('/words/{letter}/{word}', [WordsController::class, 'destroy'])->name('words.destroy');
     Route::get('/words/{letter}/{word}/request', [WordRequestController::class, 'create'])->name('words.requests.create');
     Route::post('/words/{letter}/{word}/request', [WordRequestController::class, 'store'])->name('words.requests.store');
+    Route::post('/requests/{id}/approve', [WordRequestController::class, 'approve'])->name('requests.approve');
+    Route::post('/requests/{id}/reject', [WordRequestController::class, 'reject'])->name('requests.reject');
 });
