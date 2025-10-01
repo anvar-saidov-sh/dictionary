774e51b8f139874694d4b076c55fc6f69af8d39c
anvarbeck <anvarsaidov732@gmail.com>
2025-10-01 16:10:56 +0500

    New Migration

--- database/migrations/2025_10_01_102743_create_word_requests_table.php
+++ database/migrations/2025_10_01_102743_create_word_requests_table.php
@@ -4,29 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('word_requests', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('word_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('student_id')->constrained()->cascadeOnDelete();
-            $table->string('name')->nullable();
-            $table->text('definition')->nullable();
-            $table->text('example')->nullable();
-            $table->string('synonyms')->nullable();
+            $table->foreignId('word_id')->constrained('words')->onDelete('cascade');
+            $table->foreignId('student_id')->constrained('students')->onDelete('cascade');
+            $table->text('message')->nullable();
             $table->string('status')->default('pending');
+            $table->text('definition');
+            $table->text('examples')->nullable();
+            $table->text('idioms')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('word_requests');
