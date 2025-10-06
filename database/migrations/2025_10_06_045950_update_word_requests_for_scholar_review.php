e37b7709e7cda70b82eccbd36f3211cd456b545f
anvarbeck <anvarsaidov732@gmail.com>
2025-10-06 10:10:37 +0500

    Addition

---
+++ database/migrations/2025_10_06_045950_update_word_requests_for_scholar_review.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('word_requests', function (Blueprint $table) {
+
+            $table->enum('status', [
+                'pending_owner',
+                'pending_scholar',
+                'approved',
+                'rejected'
+            ])->default('pending_owner')->change();
+            $table->foreignId('scholar_id')->nullable()->constrained('scholars')->onDelete('set null');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('word_requests', function (Blueprint $table) {
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending')->change();
+            $table->dropConstrainedForeignId('scholar_id');
+        });
+    }
+};
