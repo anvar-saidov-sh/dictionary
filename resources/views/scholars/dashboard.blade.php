71fce82092dd071d5dfe9e5f9f780f4e036973ad
anvarbeck <anvarsaidov732@gmail.com>
2025-10-08 15:29:44 +0500

    Blade

--- resources/views/scholars/dashboard.blade.php
+++ resources/views/scholars/dashboard.blade.php
@@ -7,19 +7,19 @@
             </h1>
 
             <div class="flex flex-col gap-3 mb-8">
-                <a href="{{ route('dashboard') }}"
+                <a href="{{ route('scholar.pendingwords') }}"
                     class="px-4 py-2 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600 transition">
                     Pending Created Words
                 </a>
-                <a href="{{ route('dashboard') }}"
+                <a href="{{ route('scholar.pendingrequests') }}"
                     class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
                     Pending Word Requests
                 </a>
-                <a href="{{ route('dashboard') }}"
+                <a href="{{ route('scholar.reviewedwords') }}"
                     class="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition">
-                    Reviewed Created Words
+                    Reviewed Words
                 </a>
-                <a href="{{ route('dashboard') }}"
+                <a href="{{ route('scholar.reviewedrequests') }}"
                     class="px-4 py-2 bg-teal-500 text-white rounded-lg hover:bg-teal-600 transition">
                     Reviewed Word Requests
                 </a>
